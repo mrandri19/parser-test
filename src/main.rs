706efbe2b00ffae00e33cee56a9555f3f8f68c7f
mrandri19 <andrecogno@hotmail.it>
2016-02-13 16:44:10 +0100

    First working version

--- src/main.rs
+++ src/main.rs
@@ -8,50 +8,62 @@ fn main() {
 }
 
 fn parse(input: String) -> Result<String, String> {
-    let inputStr: Vec<char> = input.replace(" ", "").chars().collect();
-
-    let mut resultStr: String;
+    let input_str: Vec<char> = input.replace(" ", "").chars().collect();
 
     let mut index = 0;
 
     // root : number expr
-    if inputStr[index].is_digit(10) {
-        incIndex(&mut index);
+    if input_str[index].is_digit(10) {
+        print!("is_is_digit #0: ");
+        inc_index(&mut index, &input_str);
     } else {
         let err_msg = format!("char: {} should be a digit", index);
         return Err(err_msg);
     }
 
-    expr(&mut index, &inputStr);
-
-    // print!("{}\n", index);
+    let result = expr(&mut index, &input_str);
+    if result.is_err() {
+        return result;
+    }
 
     Ok("da".to_string())
 }
 
-fn expr(index: &mut usize, inputStr: &Vec<char>) -> bool {
+
+// expr : operator number [expr]
+fn expr(index: &mut usize, input_str: &Vec<char>) -> Result<String, String> {
     let backtrack = *index;
 
-    if is_operator(&(*inputStr)[*index]) {
-        incIndex(index);
+    if is_operator(&(*input_str)[*index]) {
+        print!("is_operator: ");
+        if !inc_index(index, input_str) {
+            return Err("failed to increase the index".to_string());
+        }
     } else {
-        return false;
+        return Err("The first char of an expr should be an operator".to_string());
     }
 
-    if (*inputStr)[*index].is_digit(10) {
-        incIndex(index);
+    if (*input_str)[*index].is_digit(10) {
+        print!("is_digit: : ");
+        if !inc_index(index, input_str) {
+            return Err("failed to increase the index".to_string());
+        }
     } else {
-        return false;
+        return Err("The second char of an expr should be a digit".to_string());
     }
 
     // Check if we finished
-    if (*inputStr).len() == (*index) {
-        return true;
+    print!("Did we finish?\n");
+
+    let index_plus_one = &(*index + 1);
+    if finished(index_plus_one, input_str) {
+        return Ok("finished".to_string());
     } else {
-        return expr(index, inputStr);
+        return expr(index, input_str);
     }
 }
 
+// Operator /[\+\-\*\/]/
 fn is_operator(ch: &char) -> bool {
     match *ch {
         '+' | '-' | '*' | '/' => true,
@@ -59,7 +71,20 @@ fn is_operator(ch: &char) -> bool {
     }
 }
 
-fn incIndex(i: &mut usize) {
-    *i += 1;
-    print!("{}\n", *i);
+fn inc_index(index: &mut usize, input_str: &Vec<char>) -> bool {
+    *index += 1;
+    if finished(index, input_str) {
+        return false;
+    }
+    print!("{}\n", *index);
+    true
+}
+
+fn finished(index: &usize, input_str: &Vec<char>) -> bool {
+    // If it were an equal condition we could be after the penultimate char but
+    // not finished yet
+    if (*input_str).len() < (*index) {
+        return true;
+    }
+    false
 }
